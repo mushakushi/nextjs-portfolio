b79ec65d12d507375e8a0d2fd38ca828f0e99ae0
Matthew Brown <matthewbrown.classes@gmail.com>
2023-11-10 20:20:34 -0500

    finally added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     minHeight: "100%",
                 }}
             >
+                <link rel="icon" href="/favicon.ico" sizes="any" />
                 <HandleOnComplete />
                 <StyleJsxGlobal />
                 <Providers>
